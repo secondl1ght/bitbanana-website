c5ccc56e9334ac667eb8da0d361029718a15a36a
secondl1ght <secondl1ght@protonmail.com>
2023-05-26 22:53:00 -0600

    setup tailwind config and add comp alias

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte}'],
 	theme: {
-		extend: {}
+		fontSize: {
+			xs: ['12px', '16px'],
+			sm: ['14px', '20px'],
+			base: ['16px', '24px'],
+			lg: ['18px', '28px'],
+			xl: ['20px', '28px'],
+			'2xl': ['24px', '32px'],
+			'3xl': ['30px', '36px'],
+			'4xl': ['36px', '40px'],
+			'5xl': ['48px', '1'],
+			'6xl': ['60px', '1'],
+			'7xl': ['72px', '1'],
+			'8xl': ['96px', '1'],
+			'9xl': ['128px', '1']
+		},
+		extend: {
+			colors: {
+				primary: '#232533',
+				link: '#FDBE00',
+				hover: '#FFCC33',
+				body: '#FFFDF8'
+			}
+		}
 	},
 	plugins: []
 };
