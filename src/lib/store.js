9b040b3676f40a89449a4216e78d732a9e7e0aa9
Michael <michael90@protonmail.com>
2023-07-17 00:14:22 +0200

    Add Twitter & Nostr in footer

--- src/lib/store.js
+++ src/lib/store.js
@@ -2,5 +2,7 @@ import { readable } from 'svelte/store';
 
 export const links = readable({
 	github: 'https://github.com/michaelWuensch/BitBanana',
-	discord: 'https://discord.gg/Xg85BuTc9A'
+	discord: 'https://discord.gg/Xg85BuTc9A',
+	twitter: 'https://twitter.com/BitBananaApp',
+	nostr: 'https://snort.social/p/npub1dwn7wphjhrlej6ks4sktgn77w82ayq6hn6lj37ll75tav55nd3vq07xzaj'
 });
